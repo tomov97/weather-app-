551b2275b2c5e612d9610a86a728c630e63bce5a
tomov97 <100772489+tomov97@users.noreply.github.com>
2022-07-27 07:25:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ let weather = {
              + "&units=metric&appid="
              + this.apiKey
         ).then((response) => response.json())
-        .then((data) => this.displayWeather(data)); 
+        .then((data) => {
+            if (data.cod === 200) {
+            this.displayWeather(data)
+            } else {
+                alert(data.message)
+            }
+        }); 
     },
     displayWeather: function(data) {
         const { name } = data;
@@ -18,12 +24,14 @@ let weather = {
         document.querySelector(".icon").src =
          "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = temp.toFixed(1) + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h" ;
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/2050x1100/?" + name + "')"
     },
+
+    
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
     }
